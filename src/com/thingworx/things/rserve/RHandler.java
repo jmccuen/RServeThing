80912deb546501ac69e74e2caf313d9661705946
James McCuen <james.mccuen@thingworx.com>
2017-09-07 10:33:24 -0500

    Initial commit

---
+++ src/com/thingworx/things/rserve/RHandler.java
@@ -0,0 +1,344 @@
+package com.thingworx.things.rserve;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.Vector;
+import java.util.regex.Pattern;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.mozilla.javascript.Context;
+import org.rosuda.REngine.REXP;
+import org.rosuda.REngine.REXPDouble;
+import org.rosuda.REngine.REXPInteger;
+import org.rosuda.REngine.REXPList;
+import org.rosuda.REngine.REXPLogical;
+import org.rosuda.REngine.REXPMismatchException;
+import org.rosuda.REngine.REXPString;
+import org.rosuda.REngine.RList;
+import org.rosuda.REngine.Rserve.RserveException;
+
+import com.thingworx.common.utils.MetadataUtilities;
+import com.thingworx.data.util.InfoTableInstanceFactory;
+import com.thingworx.datashape.DataShape;
+import com.thingworx.entities.interfaces.IServiceProvider;
+import com.thingworx.handlers.ServiceHandlerBase;
+import com.thingworx.metadata.DataShapeDefinition;
+import com.thingworx.metadata.FieldDefinition;
+import com.thingworx.metadata.collections.FieldDefinitionCollection;
+import com.thingworx.system.managers.DataShapeManager;
+import com.thingworx.things.connected.RemoteDatabase;
+import com.thingworx.things.database.DBUtilities;
+import com.thingworx.things.database.QueryHandlerBase;
+import com.thingworx.things.database.QueryParam;
+import com.thingworx.types.BaseTypes;
+import com.thingworx.types.InfoTable;
+import com.thingworx.types.collections.ValueCollection;
+import com.thingworx.types.collections.ValueCollectionList;
+import com.thingworx.types.primitives.IPrimitiveType;
+import com.thingworx.types.primitives.InfoTablePrimitive;
+import com.thingworx.types.primitives.NumberPrimitive;
+import com.thingworx.types.primitives.StringPrimitive;
+
+
+
+public class RHandler
+  extends ServiceHandlerBase
+{
+  public static final String HANDLER_NAME = "R";
+  private static final String SCRIPT_CONFIGURATION_TABLE = "R";
+  private static final String CODE_FIELD = "code";
+  
+  
+  public InfoTable processService(IServiceProvider serviceProvider, ValueCollection params) throws Exception
+  {
+	   
+	    RServeThing RServeThing = (RServeThing)serviceProvider;
+	    InfoTable result = new InfoTable();
+	    String RCode = ((String)getServiceImplementation().getConfigurationData().getValue("R", "code"));
+	    
+	    RCode = RCode.replace("\r", "");
+	    
+	    for (Entry<String, IPrimitiveType> entry : params.entrySet()) {
+	    	if (entry.getValue().getBaseType() == BaseTypes.INFOTABLE) {
+	    		try { 
+	    			
+	    			DataShapeDefinition ds = ((InfoTable)entry.getValue().getValue()).getDataShape();
+	    			ValueCollectionList rows = ((InfoTable)entry.getValue().getValue()).getRows();
+	    			FieldDefinitionCollection fields = ds.getFields();
+	    			ArrayList<FieldDefinition> fieldList = fields.getOrderedFieldsByOrdinal();
+	    			
+	    			REXP[] itParam = new REXP[fieldList.size()];
+	    			
+	    			String[] colNames = new String[fieldList.size()];
+	    			int c = 0;
+	    			for (FieldDefinition field : fieldList) {
+	    				colNames[c] = field.getName();
+	    				int r = 0;
+	    				if (field.getBaseType() == BaseTypes.NUMBER)
+	    				{
+	    					double[] values = new double[rows.getRowCount()];
+	    					for (ValueCollection row : rows) {
+		    					values[r] = (Double)BaseTypes.ConvertToObject(row.getValue(field.getName()), field.getBaseType());
+		    					r++;
+		    				}
+	    					itParam[c] = new REXPDouble(values);
+	    				} else if (field.getBaseType() == BaseTypes.INTEGER || field.getBaseType() == BaseTypes.LONG)
+	    				{
+	    					int[] values = new int[rows.getRowCount()];
+	    					for (ValueCollection row : rows) {
+		    					values[r] = (Integer)BaseTypes.ConvertToObject(row.getValue(field.getName()), field.getBaseType());
+		    					r++;
+		    				}
+	    					itParam[c] = new REXPInteger(values);
+	    					
+	    				} else if (field.getBaseType() == BaseTypes.BOOLEAN) 
+	    				{
+	    					boolean[] values = new boolean[rows.getRowCount()];
+	    					for (ValueCollection row : rows) {
+		    					values[r] = (Boolean)BaseTypes.ConvertToObject(row.getValue(field.getName()), field.getBaseType());
+		    					r++;
+		    				}
+	    					itParam[c] = new REXPLogical(values);
+	    				} else {
+	    					String[] values = new String[rows.getRowCount()];
+	    					for (ValueCollection row : rows) {
+		    					values[r] = row.getStringValue(field.getName());
+		    					r++;
+		    				}
+	    					itParam[c] = new REXPString(values);
+	    				}
+	    				
+	    				c++;
+	    			}
+	    			
+	    			RServeThing._connection.assign(entry.getKey(), REXP.createDataFrame(new RList(itParam,colNames)));
+	    		} catch (Exception e ) {
+	    			throw new Exception("Error - " + e.getMessage());
+	    		}
+	    	} else if (entry.getValue().getBaseType() == BaseTypes.NUMBER || entry.getValue().getBaseType() == BaseTypes.LONG || entry.getValue().getBaseType() == BaseTypes.INTEGER)  {
+	    		RServeThing._connection.assign(entry.getKey(), new REXPDouble((double)entry.getValue().getValue()));
+	    	} else if (entry.getValue().getBaseType() == BaseTypes.STRING) { 
+	    		RServeThing._connection.assign(entry.getKey(), entry.getValue().getStringValue());
+	    	}
+	    }
+	  
+	   
+	    String dataShape = getServiceDefinition().getResultType().getDataShapeName();
+	    
+	    if(dataShape == null || dataShape.isEmpty()) {
+	    	dataShape = null;
+	    } else {
+	    	result = InfoTableInstanceFactory.createInfoTableFromDataShape(dataShape);
+	    }
+	    
+	    try { 
+	    	
+	    	REXP value = RServeThing._connection.parseAndEval(RCode);
+	    	
+	    	return GetResultTable(value);
+	    
+	} catch (Exception err) {
+	    	throw new Exception(err.getMessage());
+	}
+  }
+  
+ 
+ 
+public InfoTable GetResultTable(REXP value) throws Exception {
+	InfoTable result = new InfoTable();
+	
+	if (value.isList()) {
+		int c = 0;
+		Iterator<?> items = value.asList().iterator();
+		while(items.hasNext()) {
+			
+			REXP item = (REXP) items.next();
+			if (item.isList()) {
+				
+				FieldDefinition field = new FieldDefinition();
+				String fieldName = "values";
+				field.setName(fieldName);
+				field.setBaseType(BaseTypes.INFOTABLE);
+				result.addField(field);
+				
+				ValueCollection row = new ValueCollection();
+				row.SetInfoTableValue(fieldName, GetResultTable(item));	
+				result.addRow(row);
+	
+				
+			} else if (item.length() > 0) {
+				FieldDefinition field = new FieldDefinition();
+				String fieldName = "value" + (c + 1);
+				try {
+					Vector names = value.asList().names; 
+					fieldName = (String)names.get(c);
+				} catch (Exception err) {};
+				field.setName(fieldName);
+				field.setBaseType(GetBaseType(item));
+				result.addField(field);
+				
+				for (int r=0;r<item.length();r++) {
+					
+					String fieldValue = item.asStrings()[r];
+
+					if (c==0) {
+						ValueCollection row = new ValueCollection();
+						row.setValue(fieldName, BaseTypes.ConvertToPrimitive(fieldValue,GetBaseType(item)));
+						result.addRow(row);
+					} else {
+						result.getRow(r).setValue(fieldName, BaseTypes.ConvertToPrimitive(fieldValue,GetBaseType(item)));
+						
+					}
+				}
+			} else if (item.isLogical()){
+				if (c==0) { 
+					FieldDefinition field = new FieldDefinition();
+					field.setName("result");
+					field.setBaseType(BaseTypes.BOOLEAN);
+					result.addField(field);
+				}
+
+				ValueCollection row = new ValueCollection();
+				row.SetBooleanValue("result",item.asInteger());
+				result.addRow(row);
+	
+			} else if (item.isInteger()){
+				if (c==0) { 
+					FieldDefinition field = new FieldDefinition();
+					field.setName("result");
+					field.setBaseType(BaseTypes.INTEGER);
+					result.addField(field);
+				}
+				
+				ValueCollection row = new ValueCollection();
+				row.SetIntegerValue("result",item.asInteger());
+				result.addRow(row);
+
+			} else if (value.isNumeric()) {
+				if (c==0) { 
+					FieldDefinition field = new FieldDefinition();
+					field.setName("result");
+					field.setBaseType(BaseTypes.NUMBER);
+					result.addField(field);
+				}
+				ValueCollection row = new ValueCollection();
+				row.SetNumberValue("result",item.asDouble());
+				result.addRow(row);
+
+			} else {
+				if (c==0) { 
+					FieldDefinition field = new FieldDefinition();
+					field.setName("result");
+					field.setBaseType(BaseTypes.STRING);
+					result.addField(field);
+				}
+				
+				ValueCollection row = new ValueCollection();
+				row.SetStringValue("result",item.asString());
+				result.addRow(row);
+
+			}
+			c++;
+		}
+	} else if (value.isLogical()) {
+	
+		FieldDefinition field = new FieldDefinition();
+		field.setName("result");
+		field.setBaseType(BaseTypes.BOOLEAN);
+		result.addField(field);
+		
+		if (value.length() > 0) {
+			for(int i=0;i<value.length();i++) {
+				Integer item = value.asIntegers()[i];
+				ValueCollection row = new ValueCollection();
+				row.SetBooleanValue("result",item);
+				result.addRow(row);
+			}
+		} else {
+			ValueCollection row = new ValueCollection();
+			row.SetBooleanValue("result",value.asInteger());
+			result.addRow(row);
+		}
+		
+	} else if (value.isInteger()){
+		FieldDefinition field = new FieldDefinition();
+		field.setName("result");
+		field.setBaseType(BaseTypes.INTEGER);
+		result.addField(field);
+		if (value.length() > 0) {
+			for(int i=0;i<value.length();i++) {
+				Integer item = value.asIntegers()[i];
+				ValueCollection row = new ValueCollection();
+				row.SetIntegerValue("result",item);
+				result.addRow(row);
+			}
+		} else {
+			ValueCollection row = new ValueCollection();
+			row.SetIntegerValue("result",value.asInteger());
+			result.addRow(row);
+		}
+
+	} else if (value.isNumeric()) {
+		
+		FieldDefinition field = new FieldDefinition();
+		field.setName("result");
+		field.setBaseType(BaseTypes.NUMBER);
+		result.addField(field);
+		
+		if (value.length() > 0) {
+			for(int i=0;i<value.length();i++) {
+				Double item = value.asDoubles()[i];
+				ValueCollection row = new ValueCollection();
+				row.SetNumberValue("result",item);
+				result.addRow(row);
+			}
+		} else {
+			ValueCollection row = new ValueCollection();
+			row.SetNumberValue("result",value.asDouble());
+			result.addRow(row);
+		}
+	} else {
+		FieldDefinition field = new FieldDefinition();
+		field.setName("result");
+		field.setBaseType(BaseTypes.STRING);
+		result.addField(field);
+		if (value.length() > 0) {
+			for(int i=0;i<value.length();i++) {
+				String item = value.asStrings()[i];
+				ValueCollection row = new ValueCollection();
+				row.SetStringValue("result",item);
+				result.addRow(row);
+			}
+		} else {
+			
+			ValueCollection row = new ValueCollection();
+			row.SetStringValue("result",value.asString());
+			result.addRow(row);
+		}
+	}
+	
+	return result;
+		
+	}
+
+
+  
+  public BaseTypes GetBaseType(REXP fieldList) {
+	  if (fieldList.isLogical()) {
+		  return BaseTypes.BOOLEAN;
+	  } else if (fieldList.isInteger())
+	  {
+		  return BaseTypes.LONG;
+	  } else if (fieldList.isNumeric())
+	  {
+		  return BaseTypes.NUMBER;
+	  } else {
+		  return BaseTypes.STRING;
+	  }
+  }
+
+
+}
---
+++ src/com/thingworx/things/rserve/RServeThing.java
@@ -0,0 +1,78 @@
+package com.thingworx.things.rserve;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+import org.rosuda.REngine.REXP;
+import org.rosuda.REngine.Rserve.RConnection;
+import org.slf4j.Logger;
+
+import com.thingworx.data.util.InfoTableInstanceFactory;
+import com.thingworx.logging.LogUtilities;
+import com.thingworx.metadata.FieldDefinition;
+import com.thingworx.metadata.annotations.ThingworxBaseTemplateDefinition;
+import com.thingworx.metadata.annotations.ThingworxConfigurationTableDefinition;
+import com.thingworx.metadata.annotations.ThingworxConfigurationTableDefinitions;
+import com.thingworx.metadata.annotations.ThingworxDataShapeDefinition;
+import com.thingworx.metadata.annotations.ThingworxFieldDefinition;
+import com.thingworx.metadata.annotations.ThingworxHandlerDefinitions;
+import com.thingworx.metadata.annotations.ThingworxServiceDefinition;
+import com.thingworx.metadata.annotations.ThingworxServiceParameter;
+import com.thingworx.metadata.annotations.ThingworxServiceResult;
+import com.thingworx.things.connections.ConnectableThing;
+import com.thingworx.types.BaseTypes;
+import com.thingworx.types.InfoTable;
+import com.thingworx.types.collections.ValueCollection;
+
+@SuppressWarnings("serial")
+@ThingworxBaseTemplateDefinition(name = "GenericThing")
+@ThingworxConfigurationTableDefinitions(tables = {
+		@ThingworxConfigurationTableDefinition(name = "RServeConfiguration", description = "", isMultiRow = false, ordinal = 0, dataShape = @ThingworxDataShapeDefinition(fields = {
+				@ThingworxFieldDefinition(name = "host", description = "host address for the RServe Server", baseType = "STRING", ordinal = 0, aspects={"defaultValue:localhost"}),
+				@ThingworxFieldDefinition(name = "port", description = "Port for the RServe Server", baseType = "INTEGER", ordinal = 1, aspects={"defaultValue:6311"}),
+				@ThingworxFieldDefinition(name = "username", description = "", baseType = "STRING", ordinal = 2),
+				@ThingworxFieldDefinition(name = "password", description = "", baseType = "PASSWORD", ordinal = 3) })) })
+@ThingworxHandlerDefinitions(handlers={@com.thingworx.metadata.annotations.ThingworxHandlerDefinition(name="R", description="Connected R handler", isUserDefinable=true, className="com.thingworx.things.rserve.RHandler")})
+public class RServeThing extends ConnectableThing {
+	RConnection _connection;
+	
+	static Logger _logger = LogUtilities.getInstance().getApplicationLogger(RServeThing.class);
+
+
+	
+	@Override
+	public boolean isConnected() throws Exception {
+		// TODO Auto-generated method stub      
+		return this._connection.isConnected();
+	}
+	
+	public void startConnectableThing() throws Exception
+	  {
+		
+		this._connection = new RConnection((String)getConfigurationSetting("RServeConfiguration", "host"),(Integer)getConfigurationSetting("RServeConfiguration", "port"));
+		if (this._connection.isConnected()) {
+			if (this._connection.needLogin()) {
+				this._connection.login((String) getConfigurationSetting("RServeConfiguration", "username"),(String) getConfigurationSetting("RServeConfiguration", "password"));
+			}
+		}
+	    
+	  }
+	  
+	  public void stopConnectableThing() throws Exception
+	  {
+	    try
+	    {
+	      this._connection.close();
+	      this._connection.finalize();
+	      
+	    }
+	    catch (Exception eClosePool)
+	    {
+	      _logger.error("Error Closing Connection Pool in " + getName() + " [" + eClosePool.getMessage() + "]");
+	    }
+	    
+	  }
+
+	  
+	
+}
